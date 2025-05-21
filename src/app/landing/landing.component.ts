13d42da3b2d13c6bdd7453b3b8253d6417ccb614
bpagbla <pagesbeatriz@gmail.com>
2025-05-21 10:38:54 +0100

    feat: landing

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
@@ -6,6 +6,47 @@ import { Component } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
 
+
+export class LandingComponent implements OnInit {
+  texts: string[] = ['Co-escribe', 'Co-lee', 'Co-pages'];
+  currentText = '';
+  index = 0;
+  firstRunFinished = false;
+  fading = false;
+
+  ngOnInit(): void {
+    this.runInitialAnimation();
+  }
+
+  runInitialAnimation() {
+    const interval = setInterval(() => {
+      this.fadeText(() => {
+        this.currentText = this.texts[this.index];
+        this.index++;
+        if (this.index >= this.texts.length) {
+          clearInterval(interval);
+          this.firstRunFinished = true;
+        }
+      });
+    }, 2000);
+  }
+
+  fadeText(callback: () => void) {
+    this.fading = true;
+    setTimeout(() => {
+      callback();
+      this.fading = false;
+    }, 500); // Duración de la animación CSS
+  }
+
+  onHover() {
+    if (this.firstRunFinished) {
+      this.index = (this.index + 1) % this.texts.length;
+      this.fadeText(() => {
+        this.currentText = this.texts[this.index];
+      });
+    }
+  }
 }
+

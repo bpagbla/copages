d30ae58889fa7b81059623488e979d459e9fff59
bpagbla <pagesbeatriz@gmail.com>
2025-05-31 16:13:06 +0100

    fix: boton seguir deshabilitado para perfil propio

--- src/app/components/perfil-publico/perfil-publico.component.ts
+++ src/app/components/perfil-publico/perfil-publico.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UserService } from '../../services/userService/user.service';
 import { User } from '../../interfaces/user';
+import { NotificationService } from '../../services/notificationService/notification.service';
 
 @Component({
   selector: 'app-perfil-publico',
@@ -17,20 +18,30 @@ export class PerfilPublicoComponent implements OnInit {
   obras: any[] = [];
   error: string = '';
   estaSiguiendo: boolean = false;
+  currentUserId: number | null = null;
+
 
   constructor(
     private route: ActivatedRoute,
-    private userService: UserService
+    private userService: UserService,
+    private notificationService: NotificationService
   ) {}
 
-  ngOnInit() {
-    this.route.paramMap.subscribe((params) => {
-      this.nick = params.get('nick') || '';
+ngOnInit() {
+  this.userService.getUserInfo().subscribe({
+    next: (user) => {
+      this.currentUserId = user.id;
+      this.nick = this.route.snapshot.paramMap.get('nick') || '';
       if (this.nick) {
         this.cargarPerfil(this.nick);
       }
-    });
-  }
+    },
+    error: () => {
+      this.error = 'No se pudo obtener el usuario actual';
+    },
+  });
+}
+
 
   comprobarSiSigue(seguidoId: number) {
     console.log('comprobando');
@@ -40,14 +51,34 @@ export class PerfilPublicoComponent implements OnInit {
     });
   }
 
-  seguir() {
-    if (!this.usuario) return;
+seguir() {
+  if (!this.usuario) return;
 
-    this.userService.toggleSeguimiento(this.usuario.id).subscribe({
-      next: (res) => (this.estaSiguiendo = res.sigue),
-      error: (err) => console.error('Error al cambiar seguimiento:', err),
-    });
-  }
+  this.userService.toggleSeguimiento(this.usuario.id).subscribe({
+    next: (res) => {
+      this.estaSiguiendo = res.sigue;
+
+      const message = res.sigue
+        ? `Ahora sigues a ${this.usuario?.nick}`
+        : `Has dejado de seguir a ${this.usuario?.nick}`;
+
+      this.notificationService.show({
+        type: 'success',
+        title: 'Seguimiento actualizado',
+        message,
+      });
+    },
+    error: (err) => {
+      console.error('Error al cambiar seguimiento:', err);
+      this.notificationService.show({
+        type: 'error',
+        title: 'Error',
+        message: 'No se pudo actualizar el seguimiento',
+      });
+    },
+  });
+}
+
 
   cargarPerfil(nick: string) {
     this.userService.getPerfil(nick).subscribe({

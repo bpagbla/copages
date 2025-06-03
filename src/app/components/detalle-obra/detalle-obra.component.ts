373568176fcca96e860c71c50c4d56bca44950bc
bpagbla <pagesbeatriz@gmail.com>
2025-06-03 13:05:40 +0100

    fix: guardar obra desde detalle

--- src/app/components/detalle-obra/detalle-obra.component.ts
+++ src/app/components/detalle-obra/detalle-obra.component.ts
@@ -2,19 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { ObrasService } from '../../services/obrasService/obras.service';
 import { CommonModule, NgIf } from '@angular/common';
+import { NgIcon } from '@ng-icons/core';
+import { NotificationService } from '../../services/notificationService/notification.service';
+import { Obra } from '../../interfaces/obra';
 
 @Component({
   selector: 'app-detalle-obra',
   templateUrl: './detalle-obra.component.html',
   styleUrls: ['./detalle-obra.component.css'],
-  imports: [NgIf, CommonModule, RouterModule],
+  imports: [NgIf, CommonModule, RouterModule, NgIcon],
 })
 export class DetalleObraComponent implements OnInit {
-  obra: any;
+   obra!: Obra;
+  guardado = false;
 
   constructor(
     private route: ActivatedRoute,
-    private obrasService: ObrasService
+    private obrasService: ObrasService, private notificationService: NotificationService
   ) {}
 
   ngOnInit(): void {
@@ -22,5 +26,48 @@ export class DetalleObraComponent implements OnInit {
     this.obrasService.getObraDetalle(id).subscribe((data) => {
       this.obra = data;
     });
+  }
+  toggleBiblioteca(): void {
+    if (!this.obra.ID) return;
+
+    if (this.guardado) {
+      this.obrasService.eliminarDeBiblioteca(this.obra.ID).subscribe({
+        next: () => {
+          this.guardado = false;
+          this.notificationService.show({
+            type: 'info',
+            title: 'Eliminado de la biblioteca',
+            message: 'Se ha eliminado este libro de tu biblioteca',
+          });
+        },
+        error: (err) => {
+          console.error(err);
+          this.notificationService.show({
+            type: 'info',
+            title: 'Error al eliminar libro',
+            message: 'No se ha podido eliminar este libro de tu biblioteca',
+          });
+        },
+      });
+    } else {
+      this.obrasService.guardarEnBiblioteca(this.obra.ID).subscribe({
+        next: () => {
+          this.guardado = true;
+          this.notificationService.show({
+            type: 'info',
+            title: 'Guardado en la biblioteca',
+            message: 'Se ha guardado este libro en tu biblioteca',
+          });
+        },
+        error: (err) => {
+          console.error(err);
+          this.notificationService.show({
+            type: 'error',
+            title: 'Error al guardar el libro',
+            message: 'No se ha podido guardar este libro en la biblioteca',
+          });
+        },
+      });
+    }
   }
 }

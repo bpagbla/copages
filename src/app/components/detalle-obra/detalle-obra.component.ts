b4138aa768f3ef5c4a6ce6cf330014434ebcaf12
bpagbla <pagesbeatriz@gmail.com>
2025-06-03 12:56:13 +0100

    fix: router link detalle obra

--- src/app/components/detalle-obra/detalle-obra.component.ts
+++ src/app/components/detalle-obra/detalle-obra.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { ObrasService } from '../../services/obrasService/obras.service';
 import { CommonModule, NgIf } from '@angular/common';
 
@@ -7,7 +7,7 @@ import { CommonModule, NgIf } from '@angular/common';
   selector: 'app-detalle-obra',
   templateUrl: './detalle-obra.component.html',
   styleUrls: ['./detalle-obra.component.css'],
-  imports: [NgIf, CommonModule],
+  imports: [NgIf, CommonModule, RouterModule],
 })
 export class DetalleObraComponent implements OnInit {
   obra: any;
